df4a7adb5947e17637eeb72602c19e6cdb612365
noguy64 <samtammi32@gmail.com>
2025-02-19 17:34:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const path = require('path');
+import { spawn } from 'child_process';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 const PORT = process.env.PORT || 18000;
+
+// Bare Server
+const bareServerProcess = spawn(
+  'npx',
+  ['--yes', '@tomphttp/bare-server-node', '--port', '8080', '--host', 'localhost'],
+  {
+      stdio: 'inherit',
+      shell: true
+  }
+);
+
+app.use('/bare/', createProxyMiddleware({
+  target: 'http://localhost:8080',
+  changeOrigin: true,
+  pathRewrite: { '^/bare/': '/' }
+}));
+
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));

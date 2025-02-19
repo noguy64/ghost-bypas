2ab95325791c9c0c77aa55460998d34bd82bf993
noguy64 <samtammi32@gmail.com>
2025-02-19 18:48:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,16 @@
-import express from 'express';
-import path from 'path';
-import axios from 'axios';
-import { fileURLToPath } from 'url';
-import { spawn } from 'child_process';
-import { createProxyMiddleware } from 'http-proxy-middleware'
-
+const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 9853;
+const PORT = process.env.PORT || 18000;
 
-// Bare Server
-const bareServerProcess = spawn(
-  'npx',
-  ['--yes', '@tomphttp/bare-server-node', '--port', '8080', '--host', 'localhost'],
-  {
-      stdio: 'inherit',
-      shell: true
-  }
-);
-
-app.use('/bare/', createProxyMiddleware({
-  target: 'http://localhost:8080',
-  changeOrigin: true,
-  pathRewrite: { '^/bare/': '/' }
-}));
-
-// Static files setup
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// Define a route to serve the main HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);

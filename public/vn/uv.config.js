9a630bec6e38552fb11b7338172f84ed874e3092
noguy64 <samtammi32@gmail.com>
2025-02-19 17:34:41 -0500

    Update uv.config.js

--- public/vn/uv.config.js
+++ public/vn/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/vn/ontop/',
-    bare:'https://loungef2x.koyeb.app/bare/',
+    bare:'/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/vn/uv.handler.js',
